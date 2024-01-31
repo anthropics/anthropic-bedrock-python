125c7a15e0d0c86924e46cb533621dfb610da809
Robert Craigie <robert@craigie.dev>
2024-01-31 18:01:00 +0000

    chore(package): move to main anthropic-sdk-python repo

--- src/anthropic_bedrock/_version.py
+++
@@ -1,4 +0,0 @@
-# File generated from our OpenAPI spec by Stainless.
-
-__title__ = "anthropic_bedrock"
-__version__ = "0.8.0"  # x-release-please-version
--- src/anthropic_bedrock/types/__init__.py
+++
@@ -1,6 +0,0 @@
-# File generated from our OpenAPI spec by Stainless.
-
-from __future__ import annotations
-
-from .completion import Completion as Completion
-from .completion_create_params import CompletionCreateParams as CompletionCreateParams
